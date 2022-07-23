284162d11e2268f7306756708284852c811e8f3d
Vladimir Sakhonchik <Vladimir_Sakhonchik@epam.com>
2022-07-24 00:25:40 +0200

    add tasks

--- functions.sh
+++ functions.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
-
-# Put your code here
+set -x
+function pow { rez=$(($1**$2)); echo $rez; }
+function shortest {
+arr=("$@")
+shot_str=${#arr[0]}
+for (( i = 0; i < $#; i++ )) ; do
+        if (( ${#arr[$i]} == $shot_str ))
+            then
+                rez+=(${arr[$i]})
+        elif
+            (( ${#arr[$i]} < $shot_str ))
+            then
+                shot_str=${#arr[$i]}
+                rez=(); rez+=(${arr[$i]})
+        fi
+done
+    echo ${rez[*]}
+}
+function print_log (){
+    echo `date +"[%m-%d-%y %H:%M]"` $@
+}
